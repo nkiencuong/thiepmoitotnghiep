956844f143185e6f5d7aeb7901f03aec492623d1
nkiencuong <nkcuong2907@gmail.com>
2025-08-06 00:34:10 +0700

    Delete thiepmoitotnghiep directory

--- thiepmoitotnghiep/models/Message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MessageSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  message: { type: String, required: true },
-  time: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Message', MessageSchema);
